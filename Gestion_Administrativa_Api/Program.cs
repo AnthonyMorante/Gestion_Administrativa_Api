b51371785e943199bbddba9c51960ccd389392db
Anthony_Dev <morante232302@gmail.com>
2023-11-07 11:34:07 -0500

    --

--- Gestion_Administrativa_Api/Program.cs
+++ Gestion_Administrativa_Api/Program.cs
@@ -150,7 +150,6 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddWkhtmltopdf();
 var app = builder.Build();
 
-
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
