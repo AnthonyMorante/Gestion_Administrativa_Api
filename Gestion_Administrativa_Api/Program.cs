8e6c27648a3fe605a4e2e84e9afd8bddab1b767e
Juan Carlos Lozada Castillo <jc.lozadacastillo@outlook.com>
2023-12-08 18:32:48 -0500

    .net 8

--- Gestion_Administrativa_Api/Program.cs
+++ Gestion_Administrativa_Api/Program.cs
@@ -148,7 +148,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddWkhtmltopdf();
+//builder.Services.AddWkhtmltopdf();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
