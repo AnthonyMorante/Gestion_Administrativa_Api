eede26de51a0d48871181468f4e9d55cdf66740b
Juan Carlos Lozada Castillo <jc.lozadacastillo@gmail.com>
2023-12-12 17:05:50 -0500

    guardar configuraciones y validar firma

--- Gestion_Administrativa_Api/Controllers/Interfaz/ConfiguracionesController.cs
+++ Gestion_Administrativa_Api/Controllers/Interfaz/ConfiguracionesController.cs
@@ -4,6 +4,7 @@ using Gestion_Administrativa_Api.Utilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Gestion_Administrativa_Api.Controllers.Interfaz
 {
@@ -33,6 +34,7 @@ namespace Gestion_Administrativa_Api.Controllers.Interfaz
             public string? codigo { get; set; }
             public string direccionMatriz { get; set; }
             public Guid? idInformacionFirma { get; set; }
+            public string validezFirma { get; set; }
         }
 
         [HttpGet]
@@ -42,15 +44,58 @@ namespace Gestion_Administrativa_Api.Controllers.Interfaz
             {
                 var idEmpresa = Guid.Parse(Tools.getIdEmpresa(HttpContext));
                 string sql = @"SELECT  e.""idEmpresa"",e.""identificacion"",
-                            e.""razonSocial"",e.""telefono"",""ruta"",e.""idInformacionFirma"",e.""direccionMatriz""
+                            e.""razonSocial"",e.""telefono"",""ruta"",codigo,e.""idInformacionFirma"",e.""direccionMatriz""
                             FROM empresas e
                             LEFT JOIN ""informacionFirmas"" i ON i.""idInformacionFirma"" = e.""idInformacionFirma""
                             WHERE ""idEmpresa"" = @idEmpresa::UUID";
-                return Ok(await _dapper.QueryFirstOrDefaultAsync(sql, new { idEmpresa }));
+                var datos = await _dapper.QueryFirstOrDefaultAsync<datosSri>(sql, new { idEmpresa });
+                datos.validezFirma = await validezFirma(datos);
+                datos.codigo = null;
+                return Ok(datos);
             }
             catch (Exception ex)
             {
                 return Problem(ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<string> validezFirma([FromForm] datosSri _data)
+        {
+            try
+            {
+                var idEmpresa = Guid.Parse(Tools.getIdEmpresa(HttpContext));
+                if (_data.archivoFirma != null)
+                {
+                    var fullPath = $"{Tools.rootPath}/Temp/";
+                    var fileName = $"{idEmpresa}.p12";
+                    try
+                    {
+                        if (!Directory.Exists(fullPath)) Directory.CreateDirectory(fullPath);
+                        using (FileStream fs = System.IO.File.Create($"{fullPath}{fileName}")) _data.archivoFirma.CopyTo(fs);
+                        var cert = new X509Certificate2($"{fullPath}{fileName}", _data.codigo);
+                        return $"Firma valida hasta {cert.GetExpirationDateString()}";
+                    }
+                    catch (Exception ex)
+                    {
+                        return ex.Message;
+                    }
+                    finally
+                    {
+                        if (System.IO.File.Exists($"{fullPath}{fileName}")) System.IO.File.Delete($"{fullPath}{fileName}");
+                    }
+                }
+                else
+                {
+                    var rutaFirma = $"{Tools.rootPath}{_data.ruta}";
+                    if (!System.IO.File.Exists(rutaFirma)) return "No se ha registrado una firma";
+                    var cert = new X509Certificate2(rutaFirma, _data.codigo);
+                    return $"Firma valida hasta {cert.GetExpirationDateString()}";
+                }
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
             }
         }
     }
